1b308e51d8667cfd425f116b9415bbb3b7ae7e10
tana1818 <tanakagen1124@gmail.com>
2019-03-29 08:05:23 +0900

    Create to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
 
-  root 'issues#index'
-  
-  resources :users
-  resources :issues
+  devise_for :users
+  root 'issue#index'
+  resources :issue, only:[:index]
+  resources :user
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
