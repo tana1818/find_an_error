77e60e767bc0c265f036178a6988b1da8130f53b
tana1818 <tanakagen1124@gmail.com>
2019-04-02 16:06:41 +0900

    Convert html to haml

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -1,6 +1,11 @@
 class IssueController < ApplicationController
+  # before_action :move_index
 
   def index
   end
+
+  # def move_index
+  #   redirect_to action: :index unless user_signed_in?
+  # end
 
 end
