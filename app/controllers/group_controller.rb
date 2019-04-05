f67ca7edc555ed1d9803c0ee7b12bdb3d7bf31d0
tana1818 <tanakagen1124@gmail.com>
2019-04-06 00:38:01 +0900

    Add to group controller

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,2 +1,26 @@
 class GroupController < ApplicationController
+  # before_action :set_group
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(name: group_params[:name],password: group_params[:password], user_id: current_user.id)
+    redirect_to root_path
+  end
+
+  def favorite_group
+    set_group
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :password)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
