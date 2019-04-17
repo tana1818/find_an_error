6c66d6e95fa1684936ae9ec3bf2a75473e31b85f
tana1818 <tanakagen1124@gmail.com>
2019-04-17 19:34:05 +0900

    Fix to group, issue routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'issue#index'
-  resources :issue
   resources :user
-  resources :group, only: [:new, :create, :edit, :update]
-
+  resources :group, only: [:new, :create, :edit, :update] do
+    resources :issue
+    resources :category, only: [:new, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
